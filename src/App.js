a00a6abbafa0ac2665d6a995903f9ad05b5c2066
taiyeoguns <taiyeoguns@yahoo.com>
2018-09-19 22:54:32 +0100

    Modified components to display data from api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
+import _ from "lodash";
 
 import NewsItemContainer from "./components/NewsItemContainer";
 
 class App extends Component {
+  state = {
+    sources: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://newsapi.org/v2/sources?language=en&apiKey=" +
+          process.env.REACT_APP_NEWS_API_KEY
+      )
+      .then(res =>
+        this.setState({ sources: _.sampleSize(res.data.sources, 3) })
+      )
+      .catch(err => {
+        console.log(err.message);
+        alert(err.message);
+      });
+  }
+
   render() {
     const header_style = {
       textAlign: "center"
     };
 
     return (
-      <div>
+      <div className="container-fluid">
         <div className="m-5" style={header_style}>
           <h1>News App - React</h1>
         </div>
 
         <div className="d-flex justify-content-between flex-wrap m-4">
-          <NewsItemContainer item="newsitem" />
+          {this.state.sources.map((source, index) => (
+            <NewsItemContainer source={source} key={index} />
+          ))}
         </div>
       </div>
     );
--- src/components/NewsItemContainer.js
+++ src/components/NewsItemContainer.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
+import _ from "lodash";
 
 import NewsItem from "./NewsItem";
 
 export default class NewsItemContainer extends Component {
 	state = {
-		title: "from state"
+		articles: []
 	};
 
+	componentDidMount() {
+		axios
+			.get(
+				"https://newsapi.org/v2/top-headlines?sources=" +
+					this.props.source.id +
+					"&apiKey=" +
+					process.env.REACT_APP_NEWS_API_KEY
+			)
+			.then(res =>
+				this.setState({ articles: _.sampleSize(res.data.articles, 5) })
+			);
+	}
+
 	render() {
+		const containerStyle = {
+			width: "23rem"
+		};
 		return (
 			<div>
-				<div className="card mb-4">
+				<div className="card mb-4" style={containerStyle}>
 					<div className="card-header">
-						<p className="text-center mb-0 font-weight-bold text-uppercase" />
+						<p className="text-center mb-0 font-weight-bold text-uppercase">
+							{this.props.source.name}
+						</p>
 					</div>
 
 					<div className="p-3">
-						<NewsItem />
+						{this.state.articles.map((article, index) => (
+							<NewsItem item={article} key={index} />
+						))}
 					</div>
 				</div>
-				<p className="text-muted">trying bootstrap</p>
-				News Item Container - {this.state.title}, props -
-				{this.props.item}, env - {process.env.REACT_APP_NEWS_API_KEY}
 			</div>
 		);
 	}
